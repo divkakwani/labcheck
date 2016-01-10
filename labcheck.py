b96a9e05e607a084e147c9ca21fdee986123e8a1
Divyanshu Kakwani <divkakwani@gmail.com>
2016-01-10 19:16:48 +0530

    Initial version of the frontend

---
+++ labcheck.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/student')
+def display_student_page():
+    return render_template('student.html')
+
+@app.route('/admin')
+def display_admin_page():
+    return render_template('admin.html')
+
+
+if __name__ == '__main__':
+    app.run()
