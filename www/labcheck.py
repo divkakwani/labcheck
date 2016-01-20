509afe41e3440a058720f4a1f90775837fd1541f
Divyanshu Kakwani <divkakwani@gmail.com>
2016-01-20 23:27:42 +0530

    Project restructuring

--- www/labcheck.py
+++ www/labcheck.py
@@ -1,21 +1,23 @@
 from flask import Flask
 from flask import render_template
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('pages/index.html')
 
 
 @app.route('/student')
 def display_student_page():
-    return render_template('student.html')
+    return render_template('pages/student/student.html')
+
 
 @app.route('/admin')
 def display_admin_page():
-    return render_template('admin.html')
+    return render_template('pages/admin/admin.html')
 
 
 if __name__ == '__main__':
