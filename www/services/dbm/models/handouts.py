f640ac78bd3e984e4b6de89bbdd7bc8ed4f44c67
Divyanshu Kakwani <divkakwani@gmail.com>
2016-03-24 00:05:10 +0530

    use functions instead of classmethods

--- www/services/dbm/models/handouts.py
+++ www/services/dbm/models/handouts.py
@@ -1,16 +1,15 @@
 from flask import g
-from .base import * 
+from .base import *
 
 
-class Handouts:
-    table_name = "handouts"
+table_name = "handouts"
 
-    @classmethod
-    def all(cls):
-        query = "select * from handouts"
-        return select_query(g.conn, query)
 
-    @classmethod
-    def add(cls, sub, path, desc):
-        query = "insert into handouts values (%s, %s, %s)"
-        return insert_query(g.conn, query, (sub, path, desc))
+def all():
+    query = "select * from handouts"
+    return select_query(g.conn, query)
+
+
+def add(cls, sub, path, desc):
+    query = "insert into handouts values (%s, %s, %s)"
+    return insert_query(g.conn, query, (sub, path, desc))
