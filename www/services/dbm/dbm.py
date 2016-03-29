b583f1489c42f297f1056e5a5c07b598a4d5154e
Divyanshu Kakwani <divkakwani@gmail.com>
2016-03-30 01:37:03 +0530

    add announcements and program routes

--- www/services/dbm/dbm.py
+++ www/services/dbm/dbm.py
@@ -14,15 +14,40 @@ from models import programs
 from time import strftime
 
 
+"""
+Error Handlers
+"""
+
+@dbm.errorhandler(Exception)
+def handle_invalid_usage(error):
+    response = jsonify(error=1)
+    response.status_code = 410
+    return response
+
+
+"""
+URL Preprocessor
+"""
+
+@dbm.url_value_preprocessor
+def set_course(endpoint, values):
+    if values['coursecode'] not in ['12IS35', 'labcheck']:
+        raise Exception('No such course')
+    g.coursecode = values['coursecode']
+
+"""
+General request setup
+"""
 
 @dbm.before_first_request
 def before_first_request():
-    DatabaseConnector.initialize(500)
+    DatabaseConnector.initialize(['labcheck', '12IS35'], 50)
 
 
 @dbm.before_request
 def before_request():
-    g.conn = DatabaseConnector.get_connection()
+    print(g.coursecode)
+    g.conn = DatabaseConnector.get_connection(g.coursecode)
 
 
 @dbm.teardown_request
@@ -30,13 +55,17 @@ def teardown_request(exception):
     DatabaseConnector.release(g.conn)
 
 
-@dbm.route('/handouts', methods=['GET'])
-def get_handouts():
+"""
+Handouts API
+"""
+
+@dbm.route('/<coursecode>/handouts', methods=['GET'])
+def get_handouts(coursecode):
     return jsonify(results=handouts.all())
 
 
-@dbm.route('/handouts', methods=['POST'])
-def post_handouts():
+@dbm.route('/<coursecode>/handouts', methods=['POST'])
+def post_handouts(coursecode):
     # save file on sys
     path = '/home/divkakwani/PycharmProjects/labcheck/www/files/handouts/' + request.files['file'].filename
     request.files['file'].save(path)
@@ -44,27 +73,51 @@ def post_handouts():
     return jsonify(results=1)
 
 
-@dbm.route('/announcements', methods=['GET'])
-def get_announcements():
+"""
+Announcements API
+"""
+
+@dbm.route('/<coursecode>/announcements', methods=['GET'])
+def get_announcements(coursecode):
     return jsonify(results=announcements.all())
 
 
-@dbm.route('/announcements', methods=['POST'])
-def post_announcements():
+@dbm.route('/<coursecode>/announcements/<pgno>', methods=['GET'])
+def get_announcements_page(coursecode, pgno):
+    return jsonify(results=announcements.nth_page(int(pgno)))
+
+
+@dbm.route('/<coursecode>/announcements', methods=['POST'])
+def post_announcements(coursecode):
     announcements.post(request.form.get('sub'),
                        request.form.get('desc'),
                        strftime('%Y%m%d%H%M%S'))
     return jsonify(results=1)
 
 
-@dbm.route('/programs/<coursecode>/<pgmcode>', methods=['GET'])
-def get_program(coursecode, pid):
-    filepath = programs.get_stmt(coursecode, pgmcode)
-    try:
-        f = open(filepath)
-    except:
-        return FileNotFoundError
-    return f.read()
+"""
+Programs API
+"""
+
+@dbm.route('/<coursecode>/programs', methods=['GET'])
+def get_programs(coursecode):
+    return jsonify(results=programs.all())
+    
+
+@dbm.route('/<coursecode>/programs/<id>')
+def get_program_by_id(coursecode, id):
+    return jsonify(results=programs.get(id))
+
+
+@dbm.route('/<coursecode>/programs/<id>/title')
+def get_title_by_id(coursecode, id):
+    return jsonify(results=programs.title(id))
+
+
+@dbm.route('/<coursecode>/programs/<id>/stmt')
+def get_stmt_by_id(coursecode, id):
+    return jsonify(results=programs.stmt(id))
+
 
 if __name__ == '__main__':
     dbm.run(debug=True)
